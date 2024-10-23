8b471f46ac8bfa5ae2c29e440021b19ff46caf67
Matt Michnal <mattm3646@gmail.com>
2024-10-22 20:53:19 -0600

    initial commit for api wrapper gem

---
+++ lib/water-gauges.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "water-gauges/version"
+
+module WaterGauges
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/water-gauges/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module WaterGauges
+  VERSION = "0.1.0"
+end
